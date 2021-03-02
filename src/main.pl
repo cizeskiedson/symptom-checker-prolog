7cc6814b00f81978d11c1094461e233547a1a2b8
Edson Cizeski <cizeskiedson@gmail.com>
2021-03-02 17:16:22 -0300

    Diagnostico finalizado.

--- src/main.pl
+++ src/main.pl
@@ -29,7 +29,42 @@ iniciar :-
     writeln("Como ja mencionado, esse teste nao e um diagnostico, mas sim apenas uma verificaçao de sintomas."),
     writeln("Para cada pergunta responda de acordo com as instruções dadas."),
     nl,
-    questionario(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    questionario(Rinite, Asma, Dpoc, Sinusite, Tuberculose, Gripe, Faringite, Pneumonia, Bronquite, Aviaria, Suina),
+    probabilidade(ResRinite, Rinite, 6),
+    probabilidade(ResAsma, Asma, 4),
+    probabilidade(ResDpoc, Dpoc, 2),
+    probabilidade(ResSinusite, Sinusite, 5),
+    probabilidade(ResTuberculose, Tuberculose, 6),
+    probabilidade(ResGripe, Gripe, 4),
+    probabilidade(ResFaringite, Faringite, 2),
+    probabilidade(ResPneumonia, Pneumonia, 3),
+    probabilidade(ResBronquite, Bronquite, 6),
+    probabilidade(ResAviaria, Aviaria, 5),
+    probabilidade(ResSuina, Suina, 6),
+    mostraDoencasPossiveis(ResRinite, ResAsma, ResDpoc, ResSinusite, ResTuberculose, ResGripe, ResFaringite, ResPneumonia, ResBronquite, ResAviaria, ResSuina),
+    write_ln("Gostaria de visualizar o diagnóstico completo? Digite 1 para Sim ou 0 para Não."),
+    read(user_input, X),
+    completo(X, ResRinite, ResAsma, ResDpoc, ResSinusite, ResTuberculose, ResGripe, ResFaringite, ResPneumonia, ResBronquite, ResAviaria, ResSuina),
+    write_ln("=========================="),
+    write_ln("Encerrando diagnóstico vamos te levar de volta para o menu principal."),
+    write_ln("Aperte 1. para continuar, ou 0. para sair"),
+    read(Y),
+    ifMenu(Y),
+    nl,
     !.
 
 
+completo(X, ResRinite, ResAsma, ResDpoc, ResSinusite, ResTuberculose, ResGripe, ResFaringite, ResPneumonia, ResBronquite, ResAviaria, ResSuina) :-
+     X == 1,
+     mostraTodasDoencas(ResRinite, ResAsma, ResDpoc, ResSinusite, ResTuberculose, ResGripe, ResFaringite, ResPneumonia, ResBronquite, ResAviaria, ResSuina).
+
+completo(X, _, _, _, _, _, _, _, _, _, _, _) :-
+     X == 0.
+
+ifMenu(Y):-
+     Y == 0,
+     abort().
+
+ifMenu(Y):-
+     Y == 1,
+     menu().
